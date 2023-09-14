1e12d0aba630359af723970aeab213bf75ceb6a6
Haryo Sukmawanto <haryo@sukmawanto.com>
2023-09-14 23:09:45 +0200

    cleaning up sender adding explicit mime plain type

--- pi/pi_sender.py
+++ pi/pi_sender.py
@@ -7,18 +7,6 @@ LEDPANEL_DIMENSIONS = (64, 32)
 DIFF_THRESHOLD = 32
 
 camera = cv2.VideoCapture(0)  # use 0 for web camera
-
-def gen_frames():  # generate frame by frame from camera
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
             
 def get_diff_frame():
     return_val, frame1 = camera.read()
@@ -38,20 +26,14 @@ def get_diff_frame():
                     sendbuffer += str(1)
                 else:
                     sendbuffer += str(0)
-    
+    print(len(sendbuffer))
     return sendbuffer
-
-@app.route('/video_feed')
-def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 @app.route('/')
 def index():
     sendbuffer = get_diff_frame()
-    return sendbuffer
+    return Response(sendbuffer, mimetype='text/plain')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
