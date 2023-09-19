d9e099237314444117de4b574b9c96b0573eeaea
Haryo <haryo.sukmawanto@gmail.com>
2023-09-19 21:39:42 +0100

    Fixed issue with Flask+CV2. the reloader was cause

--- pi/pi_sender.py
+++ pi/pi_sender.py
@@ -8,14 +8,15 @@ DIFF_THRESHOLD = 32
 
 #camera = cv2.VideoCapture(0)  # use 0 for web camera
 #camera = cv2.VideoCapture(-1)
-camera = cv2.VideoCapture('/dev/video0')
-            
+#camera = cv2.VideoCapture('/dev/video0')
+camera = cv2.VideoCapture('/dev/video0', apiPreference=cv2.CAP_V4L2)
+
 def get_diff_frame():
     return_val, frame1 = camera.read()
     frame2 = frame1
 
     sendbuffer = ""
-
+    
     if return_val:
         return_val, frame1 = camera.read()
         diff_image = cv2.absdiff(cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY), cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY))
@@ -36,6 +37,11 @@ def index():
     sendbuffer = get_diff_frame()
     return Response(sendbuffer, mimetype='text/plain')
 
+@app.route('/test')
+def test():
+    ret, frame = camera.read()
+    print(ret, frame)
+    return Response(str(ret))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, use_reloader=False)
